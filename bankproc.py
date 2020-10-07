06489e2d54016227e158999f31bf414b186ba2c8
dmb0709 <dmb0709@gmail.com>
2020-10-07 13:04:55 +0300

    Добавил программу bankproc

---
+++ bankproc.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+#from sys import argv
+def procent (babki = 0, years = 0):
+    i = 1
+    while i <= years:
+        babki = babki + babki * 0.1
+        #print(i)
+        #print(round(babki, 2))
+        print(f'Год {i} сумма на счету {round(babki, 2)} рублей.')
+        i += 1
+years = int(input('Cколько лет копить? __ '))
+babki = int(input('Сколько денег, нищеброд? __ '))
+procent(babki, years)
