1242b7ca3fa5a2eef92462ee0b219c1f9ed4777b
dmb0709 <dmb0709@gmail.com>
2020-10-08 15:43:26 +0300

    Делаем обвязку программы шифрования

--- shifr.py
+++ shifr.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 import crypt
+from sys import argv
+def koding(pkey, opentext):
+    pass
+def enkoding(pkey, cryptotext):
+    pass
+# Операции ввода ключа и текста.
+
+if len(argv) != 4:
+    print('Введено не верное количество аргументов.')
+elif argv[1] == '--code' or argv[1] == '-c':
+    print('hello!')
