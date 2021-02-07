827a77ff1a95f9a5e2f6d0ae5709ef607166bcce
dmb0709 <dmb0709@gmail.com>
2021-02-08 01:02:12 +0300

    Решена проблема кодировки UTF-8, теперь на винде работает исправно

--- code_r.py
+++ code_r.py
@@ -1,24 +1,29 @@
 #!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
 ''' Программа выводит название региона РФ по коду с номера автомобиля
 вводим значение и получаем результат
 '''
 # Открываем файл на чтение
-f = open('short.db', 'r')
+f = open('short.db', 'r', encoding='UTF-8')
+
 d = {}
 # Читаем файл в память
 for line in f:
     kv = line.split(':')
     key = kv[0]
     value = kv[1]
-    
-    d[key]=value
-   
-# Здесь мы работаем с юзером
+
+    d[key] = value
+
+
 def vvod():
+    # Здесь мы работаем с юзером
     print("Введите код региона! 0 - для выхода")
     code = input()
     if int(code) == 0:
         exit(0)
     print(d[code])
     vvod()
+
+
 vvod()
