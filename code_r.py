4c740c260a6c8942772bf785e421aacdc4bad681
dmb0709_antyX <dmb0709@gmail.com>
2021-02-08 22:05:44 +0300

    Добавил цвета в текст вывода code_r Требует тестов на винде. TODO: сделать выход по Ctrl+C

--- code_r.py
+++ code_r.py
@@ -18,13 +18,13 @@ for line in f:
 
 def vvod():
     # Здесь мы работаем с юзером
-    print("Введите код региона! 0 - для выхода")
+    print('\033[1m' + "Введите код региона! 0 - для выхода" + '\033[0m')
     code = input()
     if code == '0':
         exit(0)
     exist_key = code in d
     if exist_key == False:
-        print('Такого кода в базе нет!')
+        print('\033[37m\033[41m' + 'Такого кода в базе нет!' + '\033[0m')
         vvod()
 
     print(d[code])
